758f854427aa8f34556061840f729043f3102848
Yashgupta-commits <yashgupta0197@gmail.com>
2025-07-03 19:14:47 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,14 @@
+import math
+# Asks the user for a number as input.
+num = float(input("enter a number: "))
+
+# do calculations
+square_root = math.sqrt(num)
+natural_log = math.log(num)
+sine_value = math.sin(num)
+
+# results
+
+print("square root:" , square_root)
+print("natural log:" , natural_log)
+print("sine value:" , sine_value)
