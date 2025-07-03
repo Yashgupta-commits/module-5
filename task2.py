afa2e664adba0c614a695c5695b445a269d0bd45
Yashgupta-commits <yashgupta0197@gmail.com>
2025-07-03 19:15:49 +0530

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,14 +1,94 @@
-import math
-# Asks the user for a number as input.
-num = float(input("enter a number: "))
+file = input("Enter text to write to the file: ")
+file = open('output.txt','w')
+writing_file = file.write('Enter text to write to the file:')
+file.close()
 
-# do calculations
-square_root = math.sqrt(num)
-natural_log = math.log(num)
-sine_value = math.sin(num)
 
-# results
+file = open('output.txt','a')
+appending_file = file.write(' Hello, python!')
 
-print("square root:" , square_root)
-print("natural log:" , natural_log)
-print("sine value:" , sine_value)
+file = open('output.txt','r')
+reading_file = file.read()
+print(reading_file)
+
+file = open('output.txt','w')
+writing_file = file.write('Data successfully written to the')
+file.close()
+
+
+
+file = open('output.txt','a')
+appending_file = file.write(' output.txt')
+
+
+file = open('output.txt','r')
+reading_file = file.read()
+print(reading_file)
+
+print()
+file = open('output.txt','w')
+writing_file = file.write('Enter additional text to append:')
+file.close()
+
+
+
+file = open('output.txt','a')
+appending_file = file.write(' learning file handling in python.')
+
+
+file = open('output.txt','r')
+reading_file = file.read()
+print(reading_file)
+
+
+file = open('output.txt','w')
+writing_file = file.write('Data successfully')
+file.close()
+
+
+
+file = open('output.txt','a')
+appending_file = file.write(' appended.')
+
+file = open('output.txt','r')
+reading_file = file.read()
+print(reading_file)
+print()
+file = open('output.txt','w')
+writing_file = file.write('final content of ')
+file.close()
+
+
+
+file = open('output.txt','a')
+appending_file = file.write('output.txt')
+
+file = open('output.txt','r')
+reading_file = file.read()
+print(reading_file)
+
+file = open('output.txt','w')
+writing_file = file.write('Hello,')
+file.close()
+
+
+
+file = open('output.txt','a')
+appending_file = file.write('python!')
+
+file = open('output.txt','r')
+reading_file = file.read()
+print(reading_file)
+
+file = open('output.txt','w')
+writing_file = file.write('Learning file')
+file.close()
+
+
+
+file = open('output.txt','a')
+appending_file = file.write('handling in python.')
+
+file = open('output.txt','r')
+reading_file = file.read()
+print(reading_file)
